27b1c7d1e716db04ef3859ff2d3d87ef6469e301
JamesRunnalls <james.runnalls@eawag.ch>
2022-06-01 18:00:57 +0200

    Add new default layers

--- src/components/layergroups/layergroups.jsx
+++ src/components/layergroups/layergroups.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import editlayers from "./img/editlayers.svg";
+import { groups } from "../../config.json";
 import drawing from "./img/drawing.svg";
 import "./layergroups.css";
 
@@ -27,74 +28,14 @@ class Group extends Component {
 class LayerGroups extends Component {
   render() {
     var { setLayerGroup } = this.props;
-    var groups = [
-      {
-        name: "Lake Geneva Algal Bloom 06.09.21",
-        description: "Some description",
-        img: editlayers,
-        data: {
-          selected: [
-            [20, 15],
-            [14, 25],
-          ],
-          center: [46.405, 6.578],
-          zoom: 11,
-          datetime: new Date(1630937100000),
-        },
-      },
-      {
-        name: "Lake Zurich 3D Model",
-        description: "Some description",
-        img: editlayers,
-        data: {
-          selected: [
-            [11, 5],
-            [11, 25],
-          ],
-          center: [47.282, 8.729],
-          zoom: 12,
-        },
-      },
-      {
-        name: "Lake Geneva 3D Model",
-        description: "Some description",
-        img: editlayers,
-        data: {
-          selected: [
-            [14, 5],
-            [14, 25],
-          ],
-          center: [46.405, 6.578],
-          zoom: 11,
-        },
-      },
-      {
-        name: "Lake Greifen 3D Model",
-        description: "Some description",
-        img: editlayers,
-        data: {
-          selected: [
-            [15, 5],
-            [15, 25],
-          ],
-          center: [47.347, 8.683],
-          zoom: 13,
-        },
-      },
-      {
-        name: "Lake Geneva Chlorophyll",
-        description: "Some description",
-        img: editlayers,
-        data: {
-          selected: [
-            [20, 15],
-            [14, 25],
-          ],
-          center: [46.405, 6.578],
-          zoom: 11,
-        },
-      },
-    ];
+    var lgroups = JSON.parse(JSON.stringify(groups));
+    lgroups = lgroups.map((g) => {
+      if ("datetime" in g.data) {
+        g.data.datetime = new Date(g.data.datetime * 1000);
+      }
+      g.img = editlayers;
+      return g;
+    });
     return (
       <div className="layergroups">
         <div className="layergroups-header">
@@ -112,7 +53,7 @@ class LayerGroups extends Component {
           setLayerGroup={setLayerGroup}
           onClick={this.props.showLayers}
         />
-        {groups.map((g) => (
+        {lgroups.map((g) => (
           <Group key={g.name} properties={g} setLayerGroup={setLayerGroup} />
         ))}
       </div>
