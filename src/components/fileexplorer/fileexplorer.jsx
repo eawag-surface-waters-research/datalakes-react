74155121fd67b0955cf609a40fff711601bca13f
JamesRunnalls <james.runnalls@eawag.ch>
2023-02-16 10:07:27 +0100

    Testing

--- src/components/fileexplorer/fileexplorer.jsx
+++ src/components/fileexplorer/fileexplorer.jsx
@@ -4,13 +4,35 @@ import axios from "axios";
 
 class FileExplorer extends Component {
   state = {
-    files: [],
+    files: [null, null, null],
   };
   async componentDidMount() {
     const { data } = await axios.get(this.props.filelist);
-    const files = data.map((d) => {
-      return { id: d.k, name: d.k };
-    });
+    const files = [];
+    
+
+    /**console.log(data);
+
+    for (let i = 0; i < data.files.length; i++) {
+      files.push({
+        id: data.files[i].k,
+        name: data.files[i].k.slice(
+          data.files[i].k.lastIndexOf("/") + 1,
+          data.files[i].k.length
+        ),
+        size: data.files[i].s,
+      });
+    }
+    for (let i = 0; i < data.folders.length; i++) {
+      files.push({
+        id: data.folders[i],
+        name: data.folders[i].slice(
+          data.folders[i].lastIndexOf("/") + 1,
+          data.folders[i].length
+        ),
+        isDir: true,
+      });
+    }**/
     this.setState({ files });
   }
   render() {
--- src/pages/files/files.jsx
+++ src/pages/files/files.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import FileExplorer from '../../components/fileexplorer/fileexplorer';
-
+import FileExplorer from "../../components/fileexplorer/fileexplorer";
 
 class Files extends Component {
   render() {
     document.title = "File Explorer - Datalakes";
-    return <FileExplorer filelist={"https://eawag-data.s3.eu-central-1.amazonaws.com/renkulab.io/lexplore/idronaut-automatic-profiler/filelist.json"} />;
+    return (
+      <div style={{height: "600px"}}>
+        <FileExplorer
+          filelist={
+            "https://eawag-data.s3.eu-central-1.amazonaws.com/renkulab.io/lexplore/idronaut-automatic-profiler/filelist.json"
+          }
+        />
+      </div>
+    );
   }
 }
-
 export default Files;
