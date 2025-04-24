d7c6074bbbb3905d86275de77c48a15f8c9e647d
James Runnalls <runnalls.james@gmail.com>
2025-04-24 11:57:44 +0200

    Add parameter input boxes

--- src/components/sliders/sliderdoublenumber.jsx
+++ src/components/sliders/sliderdoublenumber.jsx
@@ -4,37 +4,88 @@ import "./slider.css";
 
 class NumberSliderDouble extends Component {
   state = {
-    tupper: this.props.upper,
-    tlower: this.props.lower,
+    _min: this.props.lower,
+    _max: this.props.upper,
   };
+
+  updateMin = () => {
+    var { _min } = this.state;
+    var { onChangeLower, min, lower } = this.props;
+    if (Number.isNaN(_min)) {
+      onChangeLower(min);
+    } else if (_min !== lower) {
+      onChangeLower(_min);
+    }
+  };
+
+  enterMin = (event) => {
+    if (event.key === "Enter") {
+      this.updateMin();
+    }
+  };
+
+  updateMax = () => {
+    var { _max } = this.state;
+    var { onChangeUpper, max, upper } = this.props;
+    if (Number.isNaN(_max)) {
+      onChangeUpper(max);
+    } else if (_max !== upper) {
+      onChangeUpper(_max);
+    }
+  };
+
+  enterMax = (event) => {
+    if (event.key === "Enter") {
+      this.updateMax();
+    }
+  };
+
+  setMin = (event) => {
+    this.setState({ _min: parseFloat(event.target.value) });
+  };
+
+  setMax = (event) => {
+    this.setState({ _max: parseFloat(event.target.value) });
+  };
+
   onUpdate = (event) => {
-    this.setState({ tlower: event[0], tupper: event[1] });
+    this.setState({ _min: event[0], _max: event[1] });
   };
   componentDidUpdate(prevProps) {
     if (this.props.upper !== prevProps.upper && !isNaN(this.props.upper)) {
-      this.setState({ tupper: this.props.upper });
+      this.setState({ _max: this.props.upper });
     }
     if (this.props.lower !== prevProps.lower && !isNaN(this.props.lower)) {
-      this.setState({ tlower: this.props.lower });
+      this.setState({ _min: this.props.lower });
     }
   }
   render() {
-    var { tlower, tupper } = this.state;
-    var {
-      min,
-      max,
-      lower,
-      upper,
-      unit,
-      onChangeLower,
-      onChangeUpper,
-    } = this.props;
+    var { _min, _max } = this.state;
+    var { min, max, lower, upper, unit, onChangeLower, onChangeUpper } =
+      this.props;
     var availability = [[min, max]];
     return (
       <div className="datetime-selector">
         <div>
-          {tlower.toFixed(2)}
-          {unit} > {tupper.toFixed(2)}
+          <input
+            type="number"
+            value={_min}
+            step="0.1"
+            onChange={this.setMin}
+            onBlur={this.updateMin}
+            onKeyDown={this.enterMin}
+            className="input-number"
+          />
+          {unit} >{" "}
+          <input
+            type="number"
+            value={_max}
+            step="0.1"
+            onChange={this.setMax}
+            onBlur={this.updateMax}
+            onKeyDown={this.enterMax}
+            className="input-number"
+          />
           {unit}
         </div>
         <SliderSelect
