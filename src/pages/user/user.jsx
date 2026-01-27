7e5d5451145c22b6b0ad8bc11eb42449bd106c36
James Runnalls <runnalls.james@gmail.com>
2026-01-27 14:58:33 +0100

    Remove unused logins

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import GitlabLogin from './gitlablogin';
-import RenkuLogin from './renkulogin';
-import GithubLogin from './githublogin';
-import EawagLogin from './eawaglogin';
+import React, { Component } from "react";
+import GithubLogin from "./githublogin";
+import EawagLogin from "./eawaglogin";
 
 class User extends Component {
-    render() {
-        document.title = "Identities - Datalakes";
-         return ( 
-            <div>
-                <div className="user">
-                    <h1>Identities</h1>
-                    <h3>To access some Datalakes advanced features, please identify yourself using the account(s) where the dataset(s) of interest are hosted.</h3>
-                    <RenkuLogin />
-                    <GitlabLogin />
-                    <EawagLogin />
-                    <GithubLogin />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    document.title = "Identities - Datalakes";
+    return (
+      <div>
+        <div className="user">
+          <h1>Identities</h1>
+          <h3>
+            To access some Datalakes advanced features, please identify yourself
+            using the account(s) where the dataset(s) of interest are hosted.
+          </h3>
+          <GithubLogin />
+          <EawagLogin />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default User;
